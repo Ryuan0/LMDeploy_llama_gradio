ef381e9ec4f8cfe31041356fdf89efd5fa065ac0
Ryuan0 <127178561+Ryuan0@users.noreply.github.com>
2024-05-14 14:41:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import gradio as gr
 from lmdeploy import pipeline, TurbomindEngineConfig
+#####
+import os
 
+# download the model llava-v1.6-vicuna-7b to the base_path directory using git tool
+base_path = './llava-v1.6-vicuna-7b'
+os.system(f'git clone https://code.openxlab.org.cn/RYUAN0/llava-v1.6-vicuna-7b.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+######
 
 backend_config = TurbomindEngineConfig(session_len=8192) # 图片分辨率较高时请调高session_len
-# pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
-pipe = pipeline('/share/new_models/liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config)
+### pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
+# pipe = pipeline('/share/new_models/liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config)
+
+#### need to change the model path;
+pipe = pipeline(base_path, backend_config=backend_config)
 
 def model(image, text):
     if image is None:
